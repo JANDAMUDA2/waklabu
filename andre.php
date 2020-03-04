57ad78bd097da7b066ca84248528aa7fe4eed2ce
submateam <61092073+submateam@users.noreply.github.com>
2020-03-04 12:04:29 +0700

    andre

--- andre.php
+++ andre.php
@@ -5,8 +5,8 @@ echo color("green"," =================================== \n");
 echo color("green"," Claim Voucher \n");
 echo color("green"," Auto Create & Redeem Voucher \n");
 echo color("green"," =================================== \n");
-echo " Created by : Vey \n";
-echo " Version    : 555 \n";
+echo " Created by : amek rambut pirang\n";
+echo " Version    : amek ketua IPK amplas \n";
 echo " Time       : ".date('d-m-Y||H:i:s')." \n";
 echo color("green"," =================================== \n");
 
